685775353cd838e301e65cc42a2bc695e55ea8b4
GabrielBossolani <bielbossolani@hotmail.com>
2025-06-08 18:24:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,47 @@
 from flask import Flask, render_template, request, redirect, url_for
+from models import db, Tarefa
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tarefas.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
 
-# Lista de tarefas simulando um banco de dados em memória
-tarefas = []
+# Criar banco de dados com as tabelas
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def index():
+    tarefas = Tarefa.query.all()
     return render_template('index.html', tarefas=tarefas)
 
 @app.route('/criar', methods=['GET', 'POST'])
 def criar():
     if request.method == 'POST':
-        titulo = request.form['titulo']
-        descricao = request.form['descricao']
-        tarefas.append({'titulo': titulo, 'descricao': descricao})
+        nova_tarefa = Tarefa(
+            titulo=request.form['titulo'],
+            descricao=request.form['descricao']
+        )
+        db.session.add(nova_tarefa)
+        db.session.commit()
         return redirect(url_for('index'))
     return render_template('criar_tarefa.html')
 
-@app.route('/editar/<int:indice>', methods=['GET', 'POST'])
-def editar(indice):
+@app.route('/editar/<int:id>', methods=['GET', 'POST'])
+def editar(id):
+    tarefa = Tarefa.query.get_or_404(id)
     if request.method == 'POST':
-        tarefas[indice]['titulo'] = request.form['titulo']
-        tarefas[indice]['descricao'] = request.form['descricao']
+        tarefa.titulo = request.form['titulo']
+        tarefa.descricao = request.form['descricao']
+        db.session.commit()
         return redirect(url_for('index'))
-    return render_template('editar_tarefa.html', indice=indice, tarefa=tarefas[indice])
+    return render_template('editar_tarefa.html', tarefa=tarefa)
 
-@app.route('/excluir/<int:indice>')
-def excluir(indice):
-    tarefas.pop(indice)
+@app.route('/excluir/<int:id>')
+def excluir(id):
+    tarefa = Tarefa.query.get_or_404(id)
+    db.session.delete(tarefa)
+    db.session.commit()
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
